955aa49a608d3e09764af84fc99786421d811f14
huyixi <hi@huyixi.com>
2024-01-23 22:40:26 +0800

    feat: add hour and minute

--- push-wiki.sh
+++ push-wiki.sh
@@ -3,8 +3,8 @@ cd ~/i/huyixi.wiki
 # git add + git commit + git push
 gac() {
   git add ${2:-.} &&
-  git commit -m "$1" &&
+  git commit -m "chore: $(date '+%Y-%m-%d %H:%M') update" &&
   git push
 }
 
-gac "chore: Automatic daily backup"
+gac
